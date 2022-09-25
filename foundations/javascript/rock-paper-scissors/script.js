26db702b71acd1f1e8833ca760d475b06d178d66
GiuseppeRussoCoco <grcjariten@gmail.com>
2022-09-25 18:53:46 +0200

    Foundations: Rock Paper Scissors. Missing logic part

---
+++ foundations/javascript/rock-paper-scissors/script.js
@@ -0,0 +1,8 @@
+/* User insert a choice */
+
+/* Computer generates a random choice */
+/* Algorithm decides who won */ 
+/* UI tells you what happenened */
+/*Counter goes up till 5, then get reset*/
+/*at 5, tells you it won*/
+
